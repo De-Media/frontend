0885d1da4b1f1d11df3618945005ab009adc441f
Manas Vivek <manasvjbp27@gmail.com>
2023-12-09 00:55:14 +0530

    :art: Added layout

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Header from '../../components/Header';
+import React from "react";
+import Layout from "../../components/Layout";
 
 const HomePage = () => {
-	return (
-		<div>
-			<Header />
-		</div>
-	);
+  return (
+    <Layout>
+      <h1>HomePage</h1>
+    </Layout>
+  );
 };
 
 export default HomePage;
