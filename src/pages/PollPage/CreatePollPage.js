0a68030f679ceaa1903b133d117aa06b62f292d3
Aryan-BanyanCloud <d.aryan@banyancloud.io>
2023-12-09 18:35:34 +0530

    new (model): added model component and create poll form

--- src/pages/PollPage/CreatePollPage.js
+++ src/pages/PollPage/CreatePollPage.js
@@ -1,18 +1,12 @@
-import { TextField } from '@mui/material';
 import React from 'react';
 import Layout from '../../components/Layout';
+import PollForm from '../../form/PollForm/PollForm';
 
 const CreatePollPage = () => {
 	return (
 		<Layout>
 			<h2 className='mb-3'>Create Poll</h2>
-			<TextField
-				id='outlined-multiline-flexible'
-				label='Ask a question...'
-				fullWidth
-				multiline
-				rows={4}
-			/>
+			<PollForm />
 		</Layout>
 	);
 };
